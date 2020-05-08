dc5872fa9f8e6969c1f369427c2ee99f589caae3
Administrator <52603245@qq.com>
2020-05-08 16:14:27 +0800

    2020.5.8

--- venv/hello.py
+++ venv/hello.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 app=Flask(__name__)
+manager=Manager(app)
+bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -9,5 +13,13 @@ def index():
 def user(name):
     return '<h1>hello,%s!</h1>' % name
 
+@app.route('/work/<name>')
+def work(name):
+    return render_template('work.html',name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
